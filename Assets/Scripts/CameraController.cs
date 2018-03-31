6e6c240f9d0dd6f2d96be7781e496ed0818bc917
yirulz <36685131+yirulz@users.noreply.github.com>
2018-03-31 11:28:11 +1100

    Level greybox, character controller, camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Reference the player
+    public GameObject player;
+    //Vector 3 variable to offset camera
+    private Vector3 offset;
+
+
+    void Start()
+    {
+        // Set offset to current position - players position
+        offset = transform.position - player.transform.position;
+
+    }
+    void LateUpdate()
+    {
+        // Set current position to players position + offset position
+        transform.position = player.transform.position + offset;
+    }
+}
